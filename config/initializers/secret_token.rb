e873af647bed6a0a45fd36a6507de537f5b1b018
John Wang <john@johntwang.com>
2011-08-28 10:58:40 -1000

    rails generated app with modified Gemfile and .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superconf::Application.config.secret_token = '0397b3040746af63c108e985fe5839085b9aadb8397b8b5a71fde75fbeaa28f6cbf604f130ae778630ee85ae2313964ed1500b2090731d15d796e7a41b66039f'
