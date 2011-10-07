99acc518f420023058d2dfef0eecbe20838d0ccc
Jussi Sipola <jussi@wisdomspace.com>
2011-10-06 20:03:26 -1000

    Added Proposal model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110828204130) do
+ActiveRecord::Schema.define(:version => 20111007053157) do
+
+  create_table "proposals", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should have_many :proposals }
+  end
 end
