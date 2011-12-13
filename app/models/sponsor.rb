8c40af8ca1f4ba30ace42d79bb0e6389aeebef9d
John Wang <john@johntwang.com>
2011-12-12 21:17:34 -1000

    start setting up paperclip

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,4 +1,7 @@
 class Sponsor < ActiveRecord::Base
   belongs_to :event
   belongs_to :sponsorship_level
+
+  has_attached_file :logo,
+                    :styles => { :medium => "160x60>", :thumb => "180x80>" }
 end
