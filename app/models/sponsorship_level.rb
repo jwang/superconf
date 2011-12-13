4a72531a17a77b64fc0e56cedfc365cbe20eaa66
John Wang <john@johntwang.com>
2011-12-12 22:05:45 -1000

    default sort order by maximum and load data into the partial

--- app/models/sponsorship_level.rb
+++ app/models/sponsorship_level.rb
@@ -1,5 +1,8 @@
 class SponsorshipLevel < ActiveRecord::Base
   belongs_to :event
+  has_many :sponsors
   validates :maximum, :numericality => { :only_integer => true, :greater_than => 0 }
   validates :name, :maximum, :presence => true
+
+  default_scope :order => 'maximum ASC'
 end
