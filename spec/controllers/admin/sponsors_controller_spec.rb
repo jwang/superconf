d3b6a418077ad002e97806674afbf7b154ad5239
John Wang <john@johntwang.com>
2011-12-20 21:22:36 -0800

    specs fixed.
    need to switch to mocks with mocha instead of before/after.
    
    also need more meaningful specs for sponsorship levels and sponsors later.

--- spec/controllers/admin/sponsors_controller_spec.rb
+++ spec/controllers/admin/sponsors_controller_spec.rb
@@ -19,18 +19,25 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe Admin::SponsorsController do
+  before(:all) do
+    @event = Event.create(:name => "Event Name")
+  end
+
+  after(:all) do
+    @event.destroy
+  end
 
   # This should return the minimal set of attributes required to create a valid
   # Sponsor. As you add validations to Sponsor, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {:name => "sponsor", :event_id => "1"}
+    {:name => "sponsor", :event_id => @event.id}
   end
 
   describe "GET index" do
     it "assigns all sponsors as @sponsors" do
       sponsor = Sponsor.create! valid_attributes
-      get :index
+      get :index, :event_id => @event.id
       assigns(:sponsors).should eq([sponsor])
     end
   end
@@ -38,14 +45,14 @@ describe Admin::SponsorsController do
   describe "GET show" do
     it "assigns the requested sponsor as @sponsor" do
       sponsor = Sponsor.create! valid_attributes
-      get :show, :id => sponsor.id.to_s
+      get :show, :id => sponsor.id.to_s, :event_id => @event.id
       assigns(:sponsor).should eq(sponsor)
     end
   end
 
   describe "GET new" do
     it "assigns a new sponsor as @sponsor" do
-      get :new
+      get :new, :event_id => @event.id
       assigns(:sponsor).should be_a_new(Sponsor)
     end
   end
@@ -53,7 +60,7 @@ describe Admin::SponsorsController do
   describe "GET edit" do
     it "assigns the requested sponsor as @sponsor" do
       sponsor = Sponsor.create! valid_attributes
-      get :edit, :id => sponsor.id.to_s
+      get :edit, :id => sponsor.id.to_s, :event_id => @event.id
       assigns(:sponsor).should eq(sponsor)
     end
   end
@@ -62,19 +69,19 @@ describe Admin::SponsorsController do
     describe "with valid params" do
       it "creates a new Sponsor" do
         expect {
-          post :create, :sponsor => valid_attributes
+          post :create, :event_id => @event.id, :sponsor => valid_attributes
         }.to change(Sponsor, :count).by(1)
       end
 
       it "assigns a newly created sponsor as @sponsor" do
-        post :create, :sponsor => valid_attributes
+        post :create, :event_id => @event.id, :sponsor => valid_attributes
         assigns(:sponsor).should be_a(Sponsor)
         assigns(:sponsor).should be_persisted
       end
 
       it "redirects to the created sponsor" do
-        post :create, :sponsor => valid_attributes
-        response.should redirect_to(Sponsor.last)
+        post :create, :event_id => @event.id, :sponsor => valid_attributes
+        response.should redirect_to([:admin, @event, Sponsor.last])
       end
     end
 
@@ -82,14 +89,14 @@ describe Admin::SponsorsController do
       it "assigns a newly created but unsaved sponsor as @sponsor" do
         # Trigger the behavior that occurs when invalid params are submitted
         Sponsor.any_instance.stub(:save).and_return(false)
-        post :create, :sponsor => {}
+        post :create, :event_id => @event.id, :sponsor => {}
         assigns(:sponsor).should be_a_new(Sponsor)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Sponsor.any_instance.stub(:save).and_return(false)
-        post :create, :sponsor => {}
+        post :create, :event_id => @event.id, :sponsor => {}
         response.should render_template("new")
       end
     end
@@ -104,19 +111,19 @@ describe Admin::SponsorsController do
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Sponsor.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => sponsor.id, :sponsor => {'these' => 'params'}
+        put :update, :event_id => @event.id, :id => sponsor.id, :sponsor => {'these' => 'params'}
       end
 
       it "assigns the requested sponsor as @sponsor" do
         sponsor = Sponsor.create! valid_attributes
-        put :update, :id => sponsor.id, :sponsor => valid_attributes
+        put :update, :event_id => @event.id, :id => sponsor.id, :sponsor => valid_attributes
         assigns(:sponsor).should eq(sponsor)
       end
 
       it "redirects to the sponsor" do
         sponsor = Sponsor.create! valid_attributes
-        put :update, :id => sponsor.id, :sponsor => valid_attributes
-        response.should redirect_to(sponsor)
+        put :update, :event_id => @event.id, :id => sponsor.id, :sponsor => valid_attributes
+        response.should redirect_to([:admin, @event, sponsor])
       end
     end
 
@@ -125,7 +132,7 @@ describe Admin::SponsorsController do
         sponsor = Sponsor.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Sponsor.any_instance.stub(:save).and_return(false)
-        put :update, :id => sponsor.id.to_s, :sponsor => {}
+        put :update, :event_id => @event.id, :id => sponsor.id.to_s, :sponsor => {}
         assigns(:sponsor).should eq(sponsor)
       end
 
@@ -133,7 +140,7 @@ describe Admin::SponsorsController do
         sponsor = Sponsor.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Sponsor.any_instance.stub(:save).and_return(false)
-        put :update, :id => sponsor.id.to_s, :sponsor => {}
+        put :update, :event_id => @event.id, :id => sponsor.id.to_s, :sponsor => {}
         response.should render_template("edit")
       end
     end
@@ -143,14 +150,14 @@ describe Admin::SponsorsController do
     it "destroys the requested sponsor" do
       sponsor = Sponsor.create! valid_attributes
       expect {
-        delete :destroy, :id => sponsor.id.to_s
+        delete :destroy, :event_id => @event.id, :id => sponsor.id.to_s
       }.to change(Sponsor, :count).by(-1)
     end
 
     it "redirects to the sponsors list" do
       sponsor = Sponsor.create! valid_attributes
-      delete :destroy, :id => sponsor.id.to_s
-      response.should redirect_to(sponsors_url)
+      delete :destroy, :event_id => @event.id, :id => sponsor.id.to_s
+      response.should redirect_to(admin_event_sponsors_url(@event.id))
     end
   end
 
--- spec/controllers/admin/sponsorship_levels_controller_spec.rb
+++ spec/controllers/admin/sponsorship_levels_controller_spec.rb
@@ -2,30 +2,24 @@ require 'spec_helper'
 
 describe Admin::SponsorshipLevelsController do
 
+  before(:all) do
+    @event = Event.create(:name => "Event Sponsorship")
+  end
+
+  after(:all) do
+    @event.destroy
+  end
+
   describe "GET 'index'" do
     it "should be successful" do
-      get 'index'
+      get :index, :event_id => @event.id
       response.should be_success
     end
   end
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "should be successful" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
+      get :new, :event_id => @event.id
       response.should be_success
     end
   end
