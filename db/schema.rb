f4e7dca52b5b5fabb8f5d2e3f695c938283f29a8
Jussi Sipola <jussi@wisdomspace.com>
2012-01-10 12:55:46 -1000

    Moved event-specific styles to event_default.css. Need to come up with a mechanism to override this based on what the current event.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111223210159) do
+ActiveRecord::Schema.define(:version => 20111221205350) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
