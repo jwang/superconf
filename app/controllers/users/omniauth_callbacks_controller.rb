da3c724a6c6a3f4a1bbb2ba138b539b6934d70eb
LupineDev <lupinedev@gmail.com>
2012-01-14 14:04:45 -1000

    Added mikes fancy design to application layout

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  layout "application"
   def google
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
