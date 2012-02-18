c020d6a971eca1a967bb36a62c747d17cad3f951
LupineDev <lupinedev@gmail.com>
2012-02-18 07:48:13 -1000

    Backing off on omniauth for now

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   layout "application"
 
-  def failure
-    puts "request " + request.inspect
-    puts "omnifail " + env['omniauth.error'].response.inspect
-    puts "omniauth.auth" + env['omniauth.auth'].inspect
-    puts "omniauth" + env['omniauth'].inspect
-    puts "omnifail type" + env['omniauth.error.type'].inspect
-    redirect_to ("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_KEY']}&code=#{params[:code]}&redirect_uri=http://aloharubyconf-qa.herokuapp.com/users/auth/github/callback")
-  end
-
   def google
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
