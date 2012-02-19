39d89871e2f4eb54bc7a792cb67304b2c76e00f1
LupineDev <lupinedev@gmail.com>
2012-02-19 10:12:06 -1000

    Redirect old static pages to event with year

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Superconf::Application.routes.draw do
 
   # redirects for static old static pages
-  match "/cfp" => redirect("/events/1/call_for_proposals")
-  match "/sponsors" => redirect("/events/1/call_for_sponsors")
-  match "/location" => redirect("/events/1/location")
+  match "/cfp" => redirect("/events/2012/call_for_proposals")
+  match "/sponsors" => redirect("/events/2012/call_for_sponsors")
+  match "/location" => redirect("/events/2012/location")
 
   resources :sponsors, :only => [:index]
   resources :events, :only => [:index, :show] do
